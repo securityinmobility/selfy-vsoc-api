67e9bf7810a910e7555471777fbb19724f2fbbbf
valljah <haerter.valentin@gmail.com>
2024-05-03 13:05:41 +0200

    Completed locustfile.py with required endpoints
    With this update, all endpoints are now being tested.
    A new task "deviation_unknown" was added and the request
    for "deviation_known" was modified.

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -48,21 +48,40 @@ class ABUser(HttpUser):
 
 
 class AISUser(HttpUser):
+    wait_time = constant_throughput(20)
+
+    @task
+    def deviation_unknown(self):
+        request = \
+            [{"extensions":
+                {"extension-definition--d83fce45-ef58-4c6c-a3f4-1fbc32e98c6e":
+                    {
+                        "extension_type": "property-extension",
+                        "source_vehicle": "12",
+                        "source_ais": "24",
+                        "source_rsu": "55",
+                        "observable": "observable",
+                    },
+                },
+            }]
+        self.client.post("/ais/deviationUnknown", json=request)
+
     @task
     def deviation_known(self):
-        request = {
-            "extension_type": "property-extension",
-            "source_vehicle": "12",
-            "source_ais": "24",
-            "source_rsu": "55",
-            "observable": {
-                "type": "type",
-                "value": "value",
-            },
-            "id": "relationship--0ee40e86-99ce-405f-820e-d5d4e2648ca4",
-            "source_ref": "indicator--e5c3e257-031f-4df1-88a3-19bbd25acacc",
-            "target_ref": "indicator--11b76a96-5d2b-45e0-8a5a-f6994f370731",
-            "description": "An immunological algorithm detected a deviation in a real-time data set "
-                           "that is apparently similar to a previously reported deviation.",
-        }
+        request = \
+            [{"extensions":
+                  {"extension-definition--d83fce45-ef58-4c6c-a3f4-1fbc32e98c6e":
+                       {"extension_type": "property-extension",
+                        "source_vehicle": "12",
+                        "source_ais": "24",
+                        "source_rsu": "55",
+                        "observable": "observable",
+                        "id": "relationship--0ee40e86-99ce-405f-820e-d5d4e2648ca4",
+                        "source_ref": "indicator--e5c3e257-031f-4df1-88a3-19bbd25acacc",
+                        "target_ref": "indicator--11b76a96-5d2b-45e0-8a5a-f6994f370731",
+                        "description": "An immunological algorithm detected a deviation in a real-time data set "
+                                       "that is apparently similar to a previously reported deviation.",
+                        },
+                   },
+              }]
         self.client.post("/ais/deviationKnown", json=request)
