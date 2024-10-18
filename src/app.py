796d7e8c2f06adf7030b2818cff9706b4fff5da9
k-gomez <sec.gomez@protonmail.com>
2024-10-18 13:30:34 +0200

    feat: new version

--- src/app.py
+++ src/app.py
@@ -41,7 +41,7 @@ sot_endpoint = "http://127.0.0.1:4204"
 # Default
 @app.route('/')
 def index():
-    return jsonify({'SELFY VSOC': 'by THI', 'version': 'v0.4'})
+    return jsonify({'SELFY VSOC': 'by THI', 'version': 'v0.5'})
 
 
 def sota_request_update(vin, action):
